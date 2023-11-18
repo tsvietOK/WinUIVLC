fb2683a15bdd96419245559ef115efde8ce14955
Vladyslav <tsvetkov_vlad@ukr.net>
2023-11-18 20:04:35 +0200

    Add UserControl AppBarWindowPresenterStateButton

---
+++ WinUIVLC/UserControls/AppBarWindowPresenterStateButton.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace WinUIVLC.UserControls;
+
+public class AppBarWindowPresenterStateButton : AppBarButton
+{
+    public static DependencyProperty IsFullScreenProperty =
+        DependencyProperty.Register("IsFullScreenProperty", typeof(bool), typeof(AppBarWindowPresenterStateButton), null);
+    public bool IsFullScreen
+    {
+        get => (bool)GetValue(IsFullScreenProperty);
+        set
+        {
+            SetValue(IsFullScreenProperty, value);
+            Icon = IsFullScreen switch
+            {
+                true => FullScreenExitIcon,
+                false => FullScreenIcon,
+            };
+        }
+    }
+
+    private static readonly FontIcon FullScreenIcon = new() { Glyph = "\uE740" };
+    private static readonly FontIcon FullScreenExitIcon = new() { Glyph = "\uE73F" };
+}
